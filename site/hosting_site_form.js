6553716645aece4c5b8304c16baf44449b248513
Adrian Rossouw <adrian@developmentseed.org>
2008-10-27 11:43:22 +0000

    #325415 - fix error introduced in language selection. Also fixes the preview / validation issues. Patch by xiong, thanks man

--- site/hosting_site_form.js
+++ site/hosting_site_form.js
@@ -1,17 +1,16 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
-/**
- * initialize the form
- */
-    hostingSitePopulate('input[@name=platform]', 'profile');
+    /**
+    * initialize the form
+    */
+    hostingSitePopulate('profile');
     $('div.hosting-site-form-language-options').parent().hide();
-
     $('input[@name=platform]').change(function() {
-      hostingSitePopulate(this, 'profile');
+      hostingSitePopulate('profile');
       $('div.hosting-site-form-language-options').parent().hide();
     });
     $('input[@name=profile]').change(function() {
-      hostingSitePopulate(this, 'language');
+      hostingSitePopulate('language');
     }); 
   });
 }
@@ -19,13 +18,21 @@ if (Drupal.jsEnabled) {
 /**
  * populate the form elements through ajax
  */
-function hostingSitePopulate(obj, option) {
-  $(obj).parent().eq(0).append("<div id='hm-processing'>processing</div>");
+function hostingSitePopulate(option) {
+  // initialize variables
+  obj = 'input[@name=' + ((option == 'profile') ? 'platform' : 'profile') + ']:checked';
+  value = $('input[@name=' + option + ']:checked').val();
+  
+  // add loading animation gif pic
+  $(obj).parent().eq(0).append("<div id='hm-processing'></div>");
+
+  // ajax function
   var resultOptions = function (data) {
     var result = Drupal.parseJson(data);
     if (result['status'] == 'TRUE') {
       resultID = 'hosting-site-form-' + option + '-options'; 
       if (result['data']) {
+        // replace form element options
         if ($('div.' + resultID).html()) {
           $('div.' + resultID).parent().after(result['data']).remove(); 
           $('div.' + resultID).parent().show();
@@ -34,17 +41,28 @@ function hostingSitePopulate(obj, option) {
           $('input.' + resultID).after(result['data']).remove();
           $('div.' + resultID).parent().show();
         }
+        // restore choice of last time
+        $('input[@name=' + option + '][@value=' + value + ']').attr("checked", "checked");
+        // bind on-change event
         if (option == 'profile') {
-          $('input[@name=profile]').change(function() {
-            hostingSitePopulate(this, 'language');
-          }); 
+          if (($('input[@name=profile]:checked').val()) || (result['type'] != 'radios')) {
+            hostingSitePopulate('language');
+          }
+          if (result['type'] == 'radios') {
+            $('input[@name=profile]').change(function() {
+              hostingSitePopulate('language');
+            }); 
+          }
         }
       }
       else {
         $('div.' + resultID).parent().hide();
       }
     }
+    // remove the gif animation
     $('div#hm-processing').remove();
   }
-  $.get('/hosting/hosting_site_form_populate/' + option + '/' + $(obj).val(), null, resultOptions);
+  // prepare url params
+  params = (option == 'profile') ? $(obj).val() : ($(obj).val() + '/' + $('input[@name=platform]:checked').val());
+  $.get('/hosting/hosting_site_form_populate/' + option + '/' + params, null, resultOptions);
 }
