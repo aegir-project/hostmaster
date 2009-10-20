2ee35edd656deb6debe6949108c00eeb7e7c6413
Adrian Rossouw <adrian@developmentseed.org>
2009-10-20 16:33:23 +0000

    Some UI fixes. an incorrect permission check on the run button. log was relabeled view and run was relabeled queue

--- task/hosting_task.js
+++ task/hosting_task.js
@@ -3,24 +3,28 @@ hostingTaskRefreshList = function() {
   $('#hosting-task-list').prepend('<div class="hosting-overlay"><div class="hosting-throbber">Refreshing task list</div></div>');
   $.get('/hosting/tasks/' + Drupal.settings.hostingTaskRefresh.nid + '/list', null,  function(data, responseText) {
     $("#hosting-task-list").html(data.markup);
+    hostingTaskBindButtons($('#hosting-task-list'));
     setTimeout("hostingTaskRefreshList()", 30000);
    }, 'json' );
 }
 
+hostingTaskBindButtons = function(elem) {
+  $('.hosting-button-enabled', elem).filter('[href^="/node"]').click(function() {
+     var options = {
+        url : '/hosting/js' + $(this).attr('href'),
+        draggable : false,
+        width : 800,
+        height : 450
+      }
+      Drupal.modalFrame.open(options);
+      return false;
+   });
+}
 
 $(document).ready(function() {
     if (Drupal.settings.hostingTaskRefresh.nid) { 
-      setTimeout("hostingTaskRefreshList()", 30000);
+      setTimeout("hostingTaskRefreshList()", 3000);
     }
-/*     $('.hosting-button-enabled').filter('[href^="/node"]').click(function() {
-       var options = {
-          url : '/hosting/js' + $(this).attr('href'),
-          draggable : false,
-          width : 800,
-          height : 450
-        }
-        Drupal.modalFrame.open(options);
-        return false;
-     });
-*/
+   // hostingTaskBindButtons($(this));
+/* */
 });
