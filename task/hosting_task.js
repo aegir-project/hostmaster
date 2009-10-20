93d04989ec761702e596f1ebd3bb8c4b04ab8dec
Adrian Rossouw <adrian@developmentseed.org>
2009-10-20 20:43:28 +0000

    #605266 - made the hosting task queue block auto-refresh too

--- task/hosting_task.js
+++ task/hosting_task.js
@@ -1,18 +1,52 @@
 hostingTaskRefreshList = function() {
 
-  $('#hosting-task-list').prepend('<div class="hosting-overlay"><div class="hosting-throbber"></div></div>');
-  $.get('/hosting/tasks/' + Drupal.settings.hostingTaskRefresh.nid + '/list', null,  function(data, responseText) {
+  var hostingTaskListRefreshCallback = function(data, responseText) {
     // If the node has been modified, reload the whole page.
     if (Drupal.settings.hostingTaskRefresh.changed < data.changed) {
       document.location.reload();
     }
     else {
       $("#hosting-task-list").html(data.markup);
-      hostingTaskBindButtons($('#hosting-task-list'));
       setTimeout("hostingTaskRefreshList()", 30000);
     }
-   }, 'json' );
+  }
+  
+ 
+  hostingTaskAddOverlay('#hosting-task-list');
+  $.get('/hosting/tasks/' + Drupal.settings.hostingTaskRefresh.nid + '/list', null, hostingTaskListRefreshCallback , 'json' );
 }
+
+
+function hostingTaskAddOverlay(elem) {
+  $(elem).prepend('<div class="hosting-overlay"><div class="hosting-throbber"></div></div>');
+}
+
+
+function hostingTaskRefreshQueueBlock() {
+  var hostingTaskQueueRefreshCallback = function(data, responseText) {
+    // If the node has been modified, reload the whole page.
+    $("#hosting-task-queue-block").html(data.markup);
+    setTimeout("hostingTaskRefreshQueueBlock()", 30000);
+  }
+ 
+  hostingTaskAddOverlay('#hosting-task-queue-block');
+  $.get('/hosting/tasks/queue', null, hostingTaskQueueRefreshCallback , 'json' );
+}
+
+$(document).ready(function() {
+
+//  hostingTaskAddOverlay('#hosting-task-queue-block');
+  if (Drupal.settings.hostingTaskRefresh.nid) { 
+    setTimeout("hostingTaskRefreshList()", 30000);
+  }
+
+  if (Drupal.settings.hostingTaskRefresh.queueBlock == 1) {
+    setTimeout("hostingTaskRefreshQueueBlock()", 30000);
+  }
+  // hostingTaskBindButtons($(this));
+/* */
+});
+
 
 hostingTaskBindButtons = function(elem) {
   $('.hosting-button-enabled', elem).filter('[href^="/node"]').click(function() {
@@ -27,11 +61,3 @@ hostingTaskBindButtons = function(elem) {
    });
 }
 
-$(document).ready(function() {
-
-    if (Drupal.settings.hostingTaskRefresh.nid) { 
-      setTimeout("hostingTaskRefreshList()", 3000);
-    }
-   // hostingTaskBindButtons($(this));
-/* */
-});
