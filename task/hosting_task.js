862fa418e5e9675b7a77f1ea18e3c571ddbbbbe8
Adrian Rossouw <adrian@developmentseed.org>
2009-10-20 17:43:02 +0000

    #605266 - added some classes to hosting-processing, so it shows the throbber while it is busy working, and if the node changed timestamp is changed, the entire page is now reloaded so changes can take effect.

--- task/hosting_task.js
+++ task/hosting_task.js
@@ -2,9 +2,15 @@ hostingTaskRefreshList = function() {
 
   $('#hosting-task-list').prepend('<div class="hosting-overlay"><div class="hosting-throbber"></div></div>');
   $.get('/hosting/tasks/' + Drupal.settings.hostingTaskRefresh.nid + '/list', null,  function(data, responseText) {
-    $("#hosting-task-list").html(data.markup);
-    hostingTaskBindButtons($('#hosting-task-list'));
-    setTimeout("hostingTaskRefreshList()", 30000);
+    // If the node has been modified, reload the whole page.
+    if (Drupal.settings.hostingTaskRefresh.changed < data.changed) {
+      document.location.reload();
+    }
+    else {
+      $("#hosting-task-list").html(data.markup);
+      hostingTaskBindButtons($('#hosting-task-list'));
+      setTimeout("hostingTaskRefreshList()", 30000);
+    }
    }, 'json' );
 }
 
@@ -22,6 +28,7 @@ hostingTaskBindButtons = function(elem) {
 }
 
 $(document).ready(function() {
+
     if (Drupal.settings.hostingTaskRefresh.nid) { 
       setTimeout("hostingTaskRefreshList()", 3000);
     }
