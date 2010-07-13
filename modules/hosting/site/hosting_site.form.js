f4b99377d2f516e827fe1d46c6db5314242f1687
Adrian Rossouw <adrian@developmentseed.org>
2010-07-13 06:59:19 +0200

    fix some silly bugs from my end.

--- modules/hosting/site/hosting_site.form.js
+++ modules/hosting/site/hosting_site.form.js
@@ -4,114 +4,111 @@ if (Drupal.jsEnabled) {
      hostingSiteToggleOptions();
      $('div.hosting-site-field input').change( hostingSiteCheck );
   });
-}
 
-hostingSiteToggleOptions  = function() {
-  // iterate through the visible options
-  settings = Drupal.settings.hostingSiteAvailableOptions;
-  console.log(settings);
+  hostingSiteToggleOptions  = function() {
+    // iterate through the visible options
+    settings = Drupal.settings.hostingSiteAvailableOptions;
 
-  for (var key in settings) {
-    css_key = key.replace(/_/g, '-');
-    desc_id = 'div#hosting-site-field-' + css_key + '-description';
+    for (var key in settings) {
+      css_key = key.replace(/_/g, '-');
+      desc_id = 'div#hosting-site-field-' + css_key + '-description';
 
-    // generate an css id to retrieve the value, based on the field type.
-    var id = 'div#hosting-site-field-' + css_key;
-    if ($(id).hasClass('hosting-site-field-radios')) { 
-      // show and hide the visible radio options.
-      if (settings[key].length > 1) {
-        // There is more than one possible option, so we display the radio dialogs.
-        $(desc_id).hide()
-        $(id).show();
-        $(id + ' div.form-radios div.form-item').hide();
-        for (var option in settings[key]) {
-          // modify the definition to get the right css id
-          option_css_key = settings[key][option].toString().replace(/[\]\[\ _]/g, '-')
-          input_id = 'input[@name=' + key + '][@value=' + settings[key][option] + ']'
-          $(id + ' div.form-radios div#edit-' + css_key + '-' + option_css_key +'-wrapper').show();
+      // generate an css id to retrieve the value, based on the field type.
+      var id = 'div#hosting-site-field-' + css_key;
+      if ($(id).hasClass('hosting-site-field-radios')) { 
+        // show and hide the visible radio options.
+        if (settings[key].length > 1) {
+          // There is more than one possible option, so we display the radio dialogs.
+          $(desc_id).hide()
+          $(id).show();
+          $(id + ' div.form-radios div.form-item').hide();
+          for (var option in settings[key]) {
+            // modify the definition to get the right css id
+            option_css_key = settings[key][option].toString().replace(/[\]\[\ _]/g, '-')
+            input_id = 'input[@name=' + key + '][@value=' + settings[key][option] + ']'
+            $(id + ' div.form-radios div#edit-' + css_key + '-' + option_css_key +'-wrapper').show();
+          }
+
         }
-
-      }
-      else if (settings[key].length == 1) {
-        // There is only one valid option, so we select it and display it as text.
-        input_id = 'input[@name=' + key + '][@value=' + settings[key][0] + ']'
-        $(input_id).attr("checked", "checked");
-        $(id).hide();
+        else if (settings[key].length == 1) {
+          // There is only one valid option, so we select it and display it as text.
+          input_id = 'input[@name=' + key + '][@value=' + settings[key][0] + ']'
+          $(input_id).attr("checked", "checked");
+          $(id).hide();
 
 
-        // we have a special case for radios that do not want their description
-        // shown. These options have the index value 'null'.
-        if (settings[key][0] != 'null') {
-          $(desc_id).show()
-            .find('div.placeholder')
-            .contents().
-            replaceWith($(input_id).parent().text().trim());
-        }
-      }
-      else {
-        $(desc_id).hide();
-        $(id).hide();
-      }
-    }
-    else if ($(id).hasClass('hosting-site-field-textfield') || $(id).hasClass('hosting-site-field-textarea')) {
-      input_id = 'input[@name=' + key + ']'
-
-      if (settings[key] == null) {
-        // we do not want the user to be able to manipulate this value,
-        // but we need to display the default value to the user.
-        $(id).hide();
-        if ($(input_id).val().length) {
-          $(desc_id).show()
-            .find('div.placeholder')
-            .contents().
-            replaceWith($(input_id).val().trim());
-        }
-      }
-      else if ((settings[key].toString().length || (settings[key] == true)) && (settings[key] != false)) {
-        $(id).show();
-        $(desc_id).hide();
-
-        // if the field does not have a value yet
-        if (!$(input_id).val().length) {
-          // we were given a default value by the server
-          if (settings[key].length) {
-            // set the textfield to the provided default
-            $(input_id).val(settings[key])
+          // we have a special case for radios that do not want their description
+          // shown. These options have the index value 'null'.
+          if (settings[key][0] != 'null') {
+            $(desc_id).show()
+              .find('div.placeholder')
+              .contents().
+              replaceWith($(input_id).parent().text().trim());
           }
         }
+        else {
+          $(desc_id).hide();
+          $(id).hide();
+        }
       }
-      else {
-        // hide the whole field and description
-        $(desc_id).hide();
-        $(id).hide();
+      else if ($(id).hasClass('hosting-site-field-textfield') || $(id).hasClass('hosting-site-field-textarea')) {
+        input_id = 'input[@name=' + key + ']'
+
+        if (settings[key] == null) {
+          // we do not want the user to be able to manipulate this value,
+          // but we need to display the default value to the user.
+          $(id).hide();
+          if ($(input_id).val().length) {
+            $(desc_id).show()
+              .find('div.placeholder')
+              .contents().
+              replaceWith($(input_id).val().trim());
+          }
+        }
+        else if ((settings[key].toString().length || (settings[key] == true)) && (settings[key] != false)) {
+          $(id).show();
+          $(desc_id).hide();
+
+          // if the field does not have a value yet
+          if (!$(input_id).val().length) {
+            // we were given a default value by the server
+            if (settings[key].length) {
+              // set the textfield to the provided default
+              $(input_id).val(settings[key])
+            }
+          }
+        }
+        else {
+          // hide the whole field and description
+          $(desc_id).hide();
+          $(id).hide();
+        }
       }
     }
   }
-}
 
-hostingSiteCheck = function() {
-  var record = {}
+  hostingSiteCheck = function() {
+    var record = {}
 
-   $('div.hosting-site-field').each( function() {
-     // get the field name for this field.
-     var field = $(this).attr('id').replace('hosting-site-field-', '').replace(/-/g,'_');
+     $('div.hosting-site-field').each( function() {
+       // get the field name for this field.
+       var field = $(this).attr('id').replace('hosting-site-field-', '').replace(/-/g,'_');
 
-     // generate an css id to retrieve the value, based on the field type.
-     var id = 'input[@name=' + field + ']';
-     if ($(this).hasClass('hosting-site-field-radios')) { 
-       id = id + ':checked' 
-     }
-      
-     // Update the record with the right values.
-     record[field] = $(id, this).val();
-  });
-   console.log(record)
-  var resultOptions = function(data) {
-    Drupal.settings.hostingSiteAvailableOptions = data
-    hostingSiteToggleOptions();
+       // generate an css id to retrieve the value, based on the field type.
+       var id = 'input[@name=' + field + ']';
+       if ($(this).hasClass('hosting-site-field-radios')) { 
+         id = id + ':checked' 
+       }
+        
+       // Update the record with the right values.
+       record[field] = $(id, this).val();
+    });
+    var resultOptions = function(data) {
+      Drupal.settings.hostingSiteAvailableOptions = data
+      hostingSiteToggleOptions();
+    }
+    $.post('/hosting/hosting_site_form_check', record, resultOptions, 'json');
   }
-  $.post('/hosting/hosting_site_form_check', record, resultOptions, 'json');
+
 }
-
-
 
