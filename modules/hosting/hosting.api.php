b2cd0f81f75c0dcd6c1379b1a1cb88c7fd0f916b
Steven Jones <steven.jones@computerminds.co.uk>
2011-04-01 20:12:18 +0100

    Add and document hook_nodeapi_TYPE_OP().

--- modules/hosting/hosting.api.php
+++ modules/hosting/hosting.api.php
@@ -39,5 +39,40 @@ function hook_hosting_service() {
 }
 
 /**
+ * Act on nodes defined by other modules.
+ *
+ * This is a more specific version of hook_nodeapi() that includes a node type
+ * and operation being performed, in the function name. When implementing this
+ * hook you should replace TYPE with the node type and OP with the node
+ * operation you would like to be notified for. A list of possible values for OP
+ * can be found in the documentation for hook_nodeapi().
+ *
+ * This hook may help you write code that is easier to follow, but this is an
+ * hosting specific hook so you may confuse other developers that are not so
+ * familar with the internals of hosting.
+ *
+ * @param &$node
+ *   The node the action is being performed on.
+ * @param $a3
+ *   - When OP is "view", passes in the $teaser parameter from node_view().
+ *   - When OP is "validate", passes in the $form parameter from node_validate().
+ * @param $a4
+ *   - When OP is "view", passes in the $page parameter from node_view().
+ * @return
+ *   This varies depending on the operation (OP).
+ *   - The "presave", "insert", "update", "delete", "print" and "view"
+ *     operations have no return value.
+ *   - The "load" operation should return an array containing pairs
+ *     of fields => values to be merged into the node object.
+ *
+ * @see hook_nodeapi()
+ * @see hosting_nodeapi()
+ */
+function hook_nodeapi_TYPE_OP(&$node, $a3, $a4) {
+  // This is an example from hosting_nodeapi_client_delete_revision().
+  db_query('DELETE FROM {hosting_client} WHERE vid = %d', $node->vid);
+}
+
+/**
  * @} End of "addtogroup hooks".
  */
