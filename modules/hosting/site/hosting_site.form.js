0d4e1c6b3d6df7c509b23f99a8fc2d783bd85064
Steven Jones <steven.jones@computerminds.co.uk>
2011-09-17 09:24:32 +0100

    Stop re-computing jQuery objects when we don't need to.

--- modules/hosting/site/hosting_site.form.js
+++ modules/hosting/site/hosting_site.form.js
@@ -23,30 +23,31 @@ Drupal.hosting.siteFormToggleOptions = function(settings) {
 
   for (var key in settings) {
     css_key = key.replace(/_/g, '-');
-    desc_id = 'div#hosting-site-field-' + css_key + '-description';
+    $desc_id = $('div#hosting-site-field-' + css_key + '-description');
 
-    // generate an css id to retrieve the value, based on the field type.
-    var id = 'div#hosting-site-field-' + css_key;
-    if ($(id).hasClass('hosting-site-field-radios')) {
+    // Generate an css id to retrieve the value, based on the field type.
+    var $id = $('div#hosting-site-field-' + css_key);
+
+    if ($id.hasClass('hosting-site-field-radios')) {
       // show and hide the visible radio options.
       if (typeof(settings[key]) != 'object') {
-        $(id).hide();
-        $(desc_id).hide();
+        $id.hide();
+        $desc_id.hide();
       }
       else if (settings[key].length > 1) {
         // There is more than one possible option, so we display the radio dialogs.
-        $(desc_id).hide()
-        $(id).show();
-        $(id + ' div.form-radios div.form-item').hide();
+        $desc_id.hide();
+        $id.show();
+        $id.find('div.form-radios div.form-item').hide();
         checked = false
         for (var option in settings[key]) {
           // modify the definition to get the right css id
           option_css_key = settings[key][option].toString().replace(/[\]\[\ _]/g, '-')
-          input_id = 'input[@name=' + key + '][@value=' + settings[key][option] + ']'
-          $(id + ' div.form-radios div#edit-' + css_key + '-' + option_css_key +'-wrapper').show();
+          var $input_id = $('input[@name=' + key + '][@value=' + settings[key][option] + ']');
+          $id.find('div.form-radios div#edit-' + css_key + '-' + option_css_key +'-wrapper').show();
 
           // one of the visible radio options has already been checked
-          if ($(input_id).attr('checked')) {
+          if ($input_id.attr('checked')) {
             checked = true;
           }
         }
@@ -56,64 +57,64 @@ Drupal.hosting.siteFormToggleOptions = function(settings) {
       }
       else if (settings[key].length == 1) {
         // There is only one valid option, so we select it and display it as text.
-        input_id = 'input[@name=' + key + '][@value=' + settings[key][0] + ']'
-        $(input_id).attr("checked", "checked");
-        $(id).hide();
+        var $input_id = $('input[@name=' + key + '][@value=' + settings[key][0] + ']');
+        $input_id.attr("checked", "checked");
+        $id.hide();
 
 
         // we have a special case for radios that do not want their description
         // shown. These options have the index value 'null'.
         if (settings[key][0] != 'null') {
-          $(desc_id).show()
+          $desc_id.show()
             .find('div.placeholder')
             .removeClass('error')
             .contents()
-            .replaceWith($(input_id).parent().text().trim())
+            .replaceWith($input_id.parent().text().trim());
         }
       }
       else {
         // If we have any errors, for the form submit not to be possible.
         can_submit = false;
 
-        $(id).hide();
-        $(desc_id).show()
+        $id.hide();
+        $desc_id.show()
           .find('div.placeholder')
           .addClass('error')
           .contents()
-          .replaceWith('No valid choices')
+          .replaceWith('No valid choices');
       }
     }
-    else if ($(id).hasClass('hosting-site-field-textfield') || $(id).hasClass('hosting-site-field-textarea')) {
-      input_id = 'input[@name=' + key + ']'
+    else if ($id.hasClass('hosting-site-field-textfield') || $id.hasClass('hosting-site-field-textarea')) {
+      var $input_id = $('input[@name=' + key + ']');
 
       if (settings[key] == null) {
         // we do not want the user to be able to manipulate this value,
         // but we need to display the default value to the user.
-        $(id).hide();
-        if ($(input_id).val().length) {
-          $(desc_id).show()
+        $id.hide();
+        if ($input_id.val().length) {
+          $desc_id.show()
             .find('div.placeholder')
             .contents().
-            replaceWith($(input_id).val().trim());
+            replaceWith($input_id.val().trim());
         }
       }
       else if ((settings[key].toString().length || (settings[key] == true)) && (settings[key] != false)) {
-        $(id).show();
-        $(desc_id).hide();
+        $id.show();
+        $desc_id.hide();
 
         // if the field does not have a value yet
-        if (!$(input_id).val().length) {
+        if (!$input_id.val().length) {
           // we were given a default value by the server
           if (settings[key].length) {
             // set the textfield to the provided default
-            $(input_id).val(settings[key])
+            $input_id.val(settings[key])
           }
         }
       }
       else {
         // hide the whole field and description
-        $(desc_id).hide();
-        $(id).hide();
+        $desc_id.hide();
+        $id.hide();
       }
     }
   }
